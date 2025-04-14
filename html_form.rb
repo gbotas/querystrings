89ad6c35a442d05c80f94929a826849d3b85372b
gbotas <gbotas@uchicago.edu>
2025-04-14 01:17:39 +0000

    forms

---
+++ html_form.rb
@@ -0,0 +1,7 @@
+<form action="/process_url">
+  <label for="fruit_id">Fruit</label>
+  <input id="fruit_id" name="fruit">
+  <label for="color_id">Color</label>
+  <input id="color_id" name="color">
+  <button>Submit</button>
+</form>
